122ae542b32d08c2b6b2827c8da5e1ac7fdb346f
Your Name <you@example.com>
2022-04-11 12:06:04 +0300

    lab6

--- DisplayFigure.cs
+++ DisplayFigure.cs
@@ -5,24 +5,24 @@ namespace laba1_OOP
 {
     public class DisplayFigure
     {
-
-        public void Draw(IFigure figure,Bitmap btn)
+        public void Draw(IFigure figure, Bitmap btn)
         {
             if (figure != null)
                 figure.Draw(btn);
+            
         }
 
-        public void Resize(IFigure figure,int x1,int y1, int x2,int  y2)
+        public void Resize(IFigure figure, int x1, int y1, int x2, int y2)
         {
             if (figure != null)
-                figure.Resize(x1,y1,x2,y2);
+                figure.Resize(x1, y1, x2, y2);
         }
 
-        public void SetProperties(IFigure figure,Color color,int size)
+        public void SetProperties(IFigure figure, Color color, int size)
         {
             if (figure != null)
             {
-                figure.SetProperties(color,size);
+                figure.SetProperties(color, size);
             }
         }
     }
--- Serialization/Singleton.cs
+++ Serialization/Singleton.cs
@@ -5,10 +5,8 @@ namespace laba1_OOP.Plugins
     public static class Singleton
     {
         public static Serializer Serializer { get; }
-
-        public static int A { set => _a = value; }
-        private static int _a;
-        static Singleton() //статический конструктор вызывается во время первого обращения
+        
+        static Singleton()
         {
             Serializer  = new Serializer();
         }
